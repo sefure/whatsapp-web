8041bfdc7a8745baf0d57fd92b0120ca58abeb41
Sefure <sefuresen98@gmail.com>
2021-09-18 12:42:37 +0300

    first commit

---
+++ client/app/public/routes.js
@@ -0,0 +1,9 @@
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+FlowRouter.route('/', {
+  name: 'public.home',
+  action: function (params, queryParams) {
+    this.render('publicLayoutDefault', { page: 'publicPageHome' });
+  }
+});
+
